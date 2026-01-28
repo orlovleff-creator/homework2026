1e7eeea670fdadff534e99416113d6cc2eec9735
fixupper <orlovleff@gmail.com>
2026-01-28 17:03:27 +0300

    домашка про книги

--- src/main/java/fourthTask/Book.java
+++ src/main/java/fourthTask/Book.java
@@ -1,8 +1,47 @@
 package fourthTask;
 
 public class Book {
+
     private String name;
     private String author;
-    private int serialNum = 0;
-    private int pageCount = 0;
+    private int serialNum;
+    private int pageCount;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public Book(String name, String author, int serialNum) {
+        this(name, author);
+        this.serialNum = serialNum;
+    }
+
+    public Book(String name, String author, int serialNum, int pageCount) {
+        this(name, author, serialNum);
+        this.pageCount = pageCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getSerialNum() {
+        return serialNum;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + author +
+                " | серия: " + serialNum +
+                " | страниц: " + pageCount;
+    }
 }
