3d2bb021d7bfd2af72afe58c3edeac57ef7dab08
fixupper <orlovleff@gmail.com>
2026-02-03 21:36:43 +0300

    фикс формулы

--- src/main/java/fourthTask/Reader.java
+++ src/main/java/fourthTask/Reader.java
@@ -17,9 +17,9 @@ public class Reader {
     public void rateBook(Book book) {
         int score = 0;
 
-        score += book.getName() != null ? book.getName().length() : random.nextInt(10);
-        score += book.getAuthor() != null ? book.getAuthor().length() : random.nextInt(10);
-        score += book.getPageCount() != 0 ? book.getPageCount() : random.nextInt(10);
+        score += book.getName() != null ? book.getName().length() : -random.nextInt(10);
+        score += book.getAuthor() != null ? book.getAuthor().length() : -random.nextInt(10);
+        score += book.getPageCount() != 0 ? book.getPageCount() : -random.nextInt(10);
         score -= book.getSerialNum() != 0 ? book.getSerialNum() : random.nextInt(10);
 
         System.out.println("Оценка книги \"" + book.getName() + "\": " + score);
